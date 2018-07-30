0a66dc96ee31e5a9eb3342d30317c776f0a0c819
hrushigore <hrushikeshgore7@gmail.com>
2018-07-30 09:20:45 +0530

    Drag and Drop not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface month {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DD';
+  title = 'Angular Material Design';
+
+  months: month[] = [
+    {value: 'Apr18', viewValue: 'April 18'},
+    {value: 'May18', viewValue: 'May 18'},
+    {value: 'Jun18', viewValue: 'June 18'}
+  ];
+  // drop(ev) {
+  //   ev.preventDefault();
+  //   var data = ev.dataTransfer.getData("text");
+  //   ev.target.appendChild(document.getElementById(data));
+  // }
+
+  // allowDrop(ev) {
+  //   ev.preventDefault();
+  // }
+
+  // drag(ev) {
+  //   ev.dataTransfer.setData("text", ev.target.id);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDividerModule,
+  MatListModule,
+  MatIconModule,
+  MatSelectModule,
+  MatInputModule,
+  MatRadioModule
+} from '@angular/material';
+// import { NgxDnDModule } from '@swimlane/dragula'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    // NgxDnDModule
   ],
   providers: [],
   bootstrap: [AppComponent]
